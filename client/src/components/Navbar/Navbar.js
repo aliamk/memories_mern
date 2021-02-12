0de4f2183854f6a46a042145db7e8c082c2cc0f4
unknown <alia.azarian@gmail.com>
2021-02-12 13:03:11 +0000

    Installed react-router-dom, wrapped the App logo in the brandContainer style class and and added the Link and To methods for navigation

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,14 +1,19 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import { AppBar, Typography } from '@material-ui/core'
 import useStyles from './styles'
 import memories from '../../images/memories.png'
 
 const Navbar = () => {
+
     const classes = useStyles()
+
     return (
         <AppBar className={classes.appBar} position="static" color="inherit">
-            <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-            <img className={classes.image} src={memories} alt="memories" height="60" />
+            <div className={classes.brandContainer}>
+                <Typography compoennt={Link} to="/" className={classes.heading} variant="h2" align="center">Memories</Typography>
+                <img className={classes.image} src={memories} alt="memories" height="60" />
+            </div>
         </AppBar>
     )
 }
